ef9d74e593fa96e839139b4840b36e66e93be360
Boan Pasaribu <boan.pasaribu@finaccel.co>
2020-09-15 15:07:51 +0700

    fitur-1 and fitur-2

--- django/mysite/mysite/urls.py
+++ django/mysite/mysite/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
                        url(r'^admins/', include(admin.site.urls)),
-                       url(r'^polls/', include('polls.urls')),
-                       #url(r'^$', views.statusss),
+                       url(r'^fitur/', include('polls.urls')),
+                       #url(r'^$', views.fitur2),
                        )

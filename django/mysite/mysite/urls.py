d4836b6d77c6aba7ca2482d599638e573fd02efa
Boan Pasaribu <boan.pasaribu@finaccel.co>
2020-09-15 15:09:25 +0700

    hotfix

--- django/mysite/mysite/urls.py
+++ django/mysite/mysite/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-                       url(r'^admin/', include(admin.site.urls)),
+                       url(r'^admins/', include(admin.site.urls)),
                        url(r'^polls/', include('polls.urls')),
                        #url(r'^$', views.statusss),
                        )

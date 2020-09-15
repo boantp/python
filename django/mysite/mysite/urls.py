3bd979602539c7d90d73e3ea01d2c88e5bfb0993
Boan Pasaribu <boan.pasaribu@finaccel.co>
2020-09-15 16:22:32 +0700

    undo-002-and--001

--- django/mysite/mysite/urls.py
+++ django/mysite/mysite/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-                       url(r'^admins-txe001/', include(admin.site.urls)),
-                       url(r'^fitur-txe-001/', include('polls.urls')),
+                       url(r'^admins-txe001-undo-commit/', include(admin.site.urls)),
+                       url(r'^fitur-txe-001-test/', include('polls.urls')),
                        )
